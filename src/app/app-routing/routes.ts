a6d40bb20f3fe191ebb1e7d35a81b495a106ba2c
riyashah17 <rhshah171198@gmail.com>
2018-08-17 20:11:39 +0530

    Routing

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { ContactComponent } from '../contact/contact.component';
+
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,27 @@ import 'hammerjs';
 import { HomeComponent } from './home/home.component';
 import { ParticlesModule } from 'angular-particle';
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    ParticlesModule
+    ParticlesModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
